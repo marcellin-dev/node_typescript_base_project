f2fb9c6aff0ac686fb61e1f0f5f90d636654dc71
LELE MARCELLIN <66904561+marcellin-dev@users.noreply.github.com>
2025-04-01 18:50:32 +0100

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 const { connect } = mongoose;
 
 let LINK_LOCAL = "mongodb://localhost:27017/BD_facture_freemo";
-let LINK_ONLINE =
-    "mongodb+srv://marc:AcsTN9V5qUL6qUzD@cluster0.txtzh.mongodb.net/BD_facture_freemo";
 
 connect(LINK_LOCAL).then(
     () => {
